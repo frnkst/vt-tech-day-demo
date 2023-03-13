76ffc9ed0bface20c34d6430a668929e8a8f2207
Frank <frank@void0.ch>
2023-03-13 21:43:00 +0100

    small fixes

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -2,15 +2,25 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-	it('renders all breakout sessions', () => {
+	it('renders all titles of the breakout sessions', () => {
 		render(<App />);
 
-		expect(screen.getAllByText(/Title/)).toHaveLength(3);
+		expect(screen.getByRole('heading', { name: 'Feature Flags'})).toBeVisible();
+		expect(screen.getByRole('heading', { name: 'Zero Login'})).toBeVisible();
+		expect(screen.getByRole('heading', { name: 'DevSecGitOps'})).toBeVisible();
+
+		expect(screen.getAllByRole('heading', { level: 5})).toHaveLength(3);
 	});
 
 	it('displays available seats correctly', () => {
 		render(<App />);
 
 		expect(screen.getByTestId("Feature Flags")).toHaveTextContent("Available Seats: 4");
+	});
+
+	it('shows a book button', () => {
+		render(<App />);
+
+		expect(screen.getAllByRole('button', { name: 'Book'})[0]).toBeVisible();
 	});
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,40 @@
 import '@/App.css'
 
-
+const breakouts = [
+  {
+    title: "Feature Flags",
+    availableSeats: 4
+  },
+  {
+    title: "Zero Login",
+    availableSeats: 10
+  },
+  {
+    title: "DevSecGitOps",
+    availableSeats: 10
+  }
+];
 
 function App() {
-  const breakouts = [
-    {
-      title: "Feature Flags",
-      availableSeats: 4
-    },
-    {
-      title: "Zero Login",
-      availableSeats: 10
-    },
-    {
-      title: "DevSecGitOps",
-      availableSeats: 10
-    }
-  ]
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Vontobel Tech Day Demo</h1>
       </header>
       <h1>Breakout Sessions</h1>
-      {breakouts.map(breakout => (
-          <div data-testid={breakout.title} key={breakout.title}
-              className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Title: {breakout.title}</h5>
-            </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Available Seats: {breakout.availableSeats}</p>
-            <a href="#"
-               className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-              Book
-            </a>
-          </div>
-      ))}
+      <div className="flex flex-col items-center">
+        {breakouts.map(breakout => (
+            <div data-testid={breakout.title} key={breakout.title}
+                 className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-3/4 m-8">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{breakout.title}</h5>
+              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Available Seats: {breakout.availableSeats}</p>
+              <button
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Book
+              </button>
+            </div>
+        ))}
+      </div>
     </div>
   )
 }
